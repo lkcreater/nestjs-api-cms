a97542d773bf6bfc4e9e6a28fd4583e97bfe5c12
ponlawat koeisuwan <ponlawat6001@gmail.com>
2025-02-10 16:52:21 +0700

    setup okta

--- src/v1/authentication/authentication.controller.ts
+++ src/v1/authentication/authentication.controller.ts
@@ -19,31 +19,11 @@ import { EV1Constant } from '../v1.constant';
 export class AuthenticationController {
   constructor(private readonly authenticationService: AuthenticationService) {}
 
-  @Post()
-  create(@Body() createAuthenticationDto: CreateAuthenticationDto) {
-    return this.authenticationService.create(createAuthenticationDto);
-  }
-
-  @Get()
+  @Get('url')
   findAll() {
-    return this.authenticationService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authenticationService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateAuthenticationDto: UpdateAuthenticationDto,
-  ) {
-    return this.authenticationService.update(+id, updateAuthenticationDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authenticationService.remove(+id);
+    const url = this.authenticationService.getUrlOAuth();
+    return {
+      url,
+    };
   }
 }
